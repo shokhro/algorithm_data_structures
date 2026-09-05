3486da7fb351ff92320b639c74ecc87db634fb4d
Shokhrukh Djavliyev <woxadham19962005@gmail.com>
2026-09-05 16:30:06 +0500

    task findMaxAverage completed in sliding window

--- src/main/java/org/example/src/leetcode/FindMaxAverage.java
+++ src/main/java/org/example/src/leetcode/FindMaxAverage.java
@@ -2,18 +2,31 @@ package org.example.src.leetcode;
 
 public class FindMaxAverage {
     public double findMaxAverage(int[] nums, int k){
-        int[] sums = new int[nums.length];
-        sums[0] = nums[0];
-        for (int i = 1; i < sums.length; i++){
-            sums[i] = sums[i - 1] + nums[i];
+        double sum = 0;
+        for (int num : nums) {
+            sum += num;
         }
 
-        double res = nums[k-1] * 1.0 / k;
-        for (int i = k; i < sums.length; i++){
-            res = Math.max(res, (sums[i] - sums[i - k]));
+        double res = sum;
+        for (int i = k; i < nums.length; i++){
+            sum += nums[i] - nums[i - k];
+            res = Math.max(res, sum);
         }
+        return res / k;
 
-        return res;
+
+//        int[] sums = new int[nums.length];
+//        sums[0] = nums[0];
+//        for (int i = 1; i < sums.length; i++){
+//            sums[i] = sums[i - 1] + nums[i];
+//        }
+//
+//        double res = nums[k-1] * 1.0 / k;
+//        for (int i = k; i < sums.length; i++){
+//            res = Math.max(res, (sums[i] - sums[i - k]));
+//        }
+//
+//        return res;
     }
 
     public static void main(String[] args) {
@@ -22,17 +35,4 @@ public class FindMaxAverage {
         FindMaxAverage fma = new FindMaxAverage();
         System.out.println(fma.findMaxAverage(nums, k));
     }
-
-    //        int[] sums = new int[nums.length];
-    //        sums[0] = nums[0];
-    //        for (int i = 1; i < nums.length; i++) {
-    //            sums[i] = sums[i-1] + nums[i];
-    //        }
-    //
-    //        int maxSum = sums[k-1];
-    //        for (int i = k; i < nums.length; i++) {
-    //            int windowSum = sums[i] - sums[i-k];
-    //            maxSum = Math.max(maxSum, windowSum);
-    //        }
-    //        return (double) maxSum / k;
 }
