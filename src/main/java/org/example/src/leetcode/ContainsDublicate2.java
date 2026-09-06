9a76575272679b508ec23d01a771c67052111b21
Shokhrukh Djavliyev <woxadham19962005@gmail.com>
2026-09-06 12:34:52 +0500

    Contains Duplicate 2 issue completed.

---
+++ src/main/java/org/example/src/leetcode/ContainsDublicate2.java
@@ -0,0 +1,22 @@
+package org.example.src.leetcode;
+
+import java.util.HashMap;
+
+public class ContainsDublicate2 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+
+            if(map.containsKey(nums[i])){
+                int oldIndex = map.get(nums[i]);
+
+                if(i - oldIndex <= k){
+                    return true;
+                }
+            }
+
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
