0b653835b7516cd0d2b9059e2171b77aada7addc
Shokhrukh Djavliyev <woxadham19962005@gmail.com>
2026-09-07 19:17:45 +0500

    insert at the beginning

--- src/main/java/org/example/src/robertSedgewick/sort/linkedList/Node.java
+++ src/main/java/org/example/src/robertSedgewick/sort/linkedList/Node.java
@@ -6,16 +6,31 @@ public class Node {
 
     public Node () {}
 
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
     public static void main(String[] args) {
         Node first = new Node();
         first.item = "to";
-
         Node second = new Node();
         second.item = "be";
         first.next = second;
-
         Node third = new Node();
         third.item = "or";
         second.next = third;
+
+        System.out.println(first.item);
+        System.out.println(first.next.item);
+
+        //insert at the beginning
+        Node oldFirst = first;
+        first = new Node();
+        first.item = "not";
+        first.next = oldFirst;
+        System.out.println("================================");
+        System.out.println(first.item);
+        System.out.println(first.next.item);
     }
 }
